2a52218af0c916a80d503fbcb5a19e6bb4e7430e
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-11-12 18:34:59 -0600

    client-feat: Inicio del proyecto, Configuración TailwindCSS

---
+++ client/crud-contacts/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="italic text-primary p-10 font-bold">Agenda de Contactos</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
