cc02290dda6a84bee2f6273d5ae8f3dfc1ebad30
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-11-14 12:18:03 -0600

    server-feat: Conexión a MySQL y Método Post

--- server/index.js
+++ server/index.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const cors = require('cors');
+const mysql = require('mysql2');
+require('dotenv').config();
 
+const password = process.env.DB_PASSWORD;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Connection to MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: password,
+  database: 'crud_contacts'
+});
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello MERN')
 })
+
+app.post('/contacts', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone_number = req.body.phone_number;
+  const genre = req.body.genre;
+
+  db.query('INSERT INTO contact_list (name,email,phone_number,genre) VALUES(?,?,?,?)', [name, email, phone_number, genre],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send('Add Contact')
+      }
+    })
+})
+
 
 const PORT = process.env.PORT ?? 3000;
 
