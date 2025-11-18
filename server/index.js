78413e2b57fed806eeea395d9e2a452bb7392e7c
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-11-17 19:52:05 -0600

    server-feat: Método Patch

--- server/index.js
+++ server/index.js
@@ -42,6 +42,33 @@ app.get('/contacts', (req, res) => {
     (err, result) => {
       if (err) {
         console.log('Data Not Found', err);
+
+        return res.status(500).json(
+          {
+            message: 'Error al obtener contactos desde la base de datos',
+            error: err.message
+          }
+        )
+      } else {
+        res.send(result)
+      }
+    }
+  )
+})
+
+app.patch('/contacts/:id', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone_number = req.body.phone_number;
+  const genre = req.body.genre;
+
+  db.query('UPDATE contact_list SET name=?,email=?,phone_number=?,genre=? WHERE id=?',
+    [name, email, phone_number, genre, id],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err)
+        console.log('Contact No Update');
       } else {
         res.send(result)
       }
